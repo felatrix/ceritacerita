9bfc05982184f48c22d2720c3d2e367488f4c6de
Iwan Darmawan <iwansaila@gmail.com>
2018-08-26 18:20:16 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Its work');
+}); 
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,
+()=>{
+    console.log(`server running on  ${port}` );
+})
