c7252730582d36f8cc16cee10f189b59a7c8b435
Iwan Darmawan <iwansaila@gmail.com>
2018-08-26 19:47:03 +0700

    authenticate request

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/google',passport.authenticate('google',{
-    scope: ['profile','email']}))
+    router.get('/google',passport.authenticate('google',{
+    scope: ['profile','email']}));
+
+    router.get('/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+        });
 
 module.exports = router;
